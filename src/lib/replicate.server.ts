f21eabc63c0f9270282065329d391ce2216c2be3
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2023-01-13 01:15:30 +0100

    [fix]: webhook

--- src/lib/replicate.server.ts
+++ src/lib/replicate.server.ts
@@ -133,9 +133,9 @@ export async function runPrediction(
 			input: {
 				prompt,
 				negative_prompt: negativePrompt,
-				...(seed && !isNaN(parseInt(seed)) ? { seed: parseInt(seed) } : {}),
-				webhook_completed: `${PRIVATE_WEBHOOK_ROOT}/api/webhooks/${user.id}/training_complete`
+				...(seed && !isNaN(parseInt(seed)) ? { seed: parseInt(seed) } : {})
 			},
+			webhook_completed: `${PRIVATE_WEBHOOK_ROOT}/api/webhooks/${user.id}/training_complete`,
 			version
 		},
 		method: 'POST'
